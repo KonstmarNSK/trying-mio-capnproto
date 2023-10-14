ba1323403d3c59b73d5512c0a98d5bc73365bc72
konstantin <konstmar@yandex.ru>
2023-10-14 19:45:40 +0700

    Added mio

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,15 +1,60 @@
+use capnp::io::Write;
 use capnp::serialize_packed;
+use std::error::Error;
+use mio::net::{TcpListener, TcpStream};
+use mio::{Events, Interest, Poll, Token};
 
 mod point_capnp {
     include!(concat!("generated/capnp", "/point_capnp.rs"));
 }
 
-fn main() -> ::capnp::Result<()>{
+const CLIENT: Token = Token(1);
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut message = ::capnp::message::Builder::new_default();
     let mut point = message.init_root::<point_capnp::point::Builder>();
     point.set_x(10.0f32);
     point.set_y(12.4f32);
 
     serialize_packed::write_message(&mut ::std::io::stdout(), &message)?;
-    Ok(())
+
+
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(128);
+
+    let addr = "127.0.0.1:13265".parse()?;
+    let mut client = TcpStream::connect(addr)?;
+    poll.registry()
+        .register(&mut client, CLIENT, Interest::READABLE | Interest::WRITABLE)?;
+
+
+    loop {
+        // Poll Mio for events, blocking until we get an event.
+        poll.poll(&mut events, None)?;
+
+        // Process each event.
+        for event in events.iter() {
+            // We can use the token we previously provided to `register` to
+            // determine for which socket the event is.
+            match event.token() {
+                CLIENT => {
+                    if event.is_writable() {
+                        // We can (likely) write to the socket without blocking.
+                        client.write_all(b"Some string");
+                        println!("Client wrote!");
+                    }
+
+                    if event.is_readable() {
+                        // We can (likely) read from the socket without blocking.
+                    }
+
+                    // Since the server just shuts down the connection, let's
+                    // just exit from our event loop.
+                    return Ok(());
+                }
+                // We don't expect any events with tokens other than those we provided.
+                _ => unreachable!(),
+            }
+        }
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,50 @@
-pub fn main() {
-    print!("Me server!")
+use std::error::Error;
+use mio::net::{TcpListener, TcpStream};
+use mio::{Events, Interest, Poll, Token};
+
+mod point_capnp {
+    include!(concat!("generated/capnp", "/point_capnp.rs"));
+}
+
+const SERVER: Token = Token(0);
+
+pub fn main() -> Result<(), Box<dyn Error>> {
+    println!("Me server!");
+
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(128);
+
+    let addr = "127.0.0.1:13265".parse()?;
+    let mut server = TcpListener::bind(addr)?;
+
+    poll.registry()
+        .register(&mut server, SERVER, Interest::READABLE)?;
+
+    println!("Server starting...");
+
+    // Start an event loop.
+    loop {
+        // Poll Mio for events, blocking until we get an event.
+        poll.poll(&mut events, None)?;
+
+        // Process each event.
+        for event in events.iter() {
+            // We can use the token we previously provided to `register` to
+            // determine for which socket the event is.
+            match event.token() {
+                SERVER => {
+                    // If this is an event for the server, it means a connection
+                    // is ready to be accepted.
+                    //
+                    // Accept the connection and drop it immediately. This will
+                    // close the socket and notify the client of the EOF.
+                    let connection = server.accept();
+                    println!("Server accepted!");
+                    drop(connection);
+                }
+                // We don't expect any events with tokens other than those we provided.
+                _ => unreachable!(),
+            }
+        }
+    }
 }
